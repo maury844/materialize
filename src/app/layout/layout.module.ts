a1fc3575c353abd0c66f0891388dc1f124eaacf4
Mauricio Fernandez <maury844@gmail.com>
2020-02-29 16:36:55 -0400

    Add footer

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, FooterComponent],
   imports: [CommonModule, MDBBootstrapModule.forRoot(), RouterModule],
-  exports: [NavbarComponent],
+  exports: [NavbarComponent, MDBBootstrapModule, FooterComponent],
 })
 export class LayoutModule {}
