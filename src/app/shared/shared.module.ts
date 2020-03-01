c2106d68c06c196ad3e6cd8a2f5396e7e253607a
friendou <robert.dmc@hotmail.com>
2020-03-01 00:30:34 -0400

    Adding export for product card comp

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImageDescriptionCardComponent } from './components/image-description-card/image-description-card.component';
 import { LayoutModule } from '../layout/layout.module';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 
 @NgModule({
-  declarations: [ImageDescriptionCardComponent],
+  declarations: [ImageDescriptionCardComponent, ProductCardComponent],
   imports: [CommonModule, LayoutModule],
-  exports: [ImageDescriptionCardComponent],
+  exports: [ImageDescriptionCardComponent, ProductCardComponent],
 })
 export class SharedModule {}
