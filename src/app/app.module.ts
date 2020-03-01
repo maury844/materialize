1e0914f0a1a31eb67095d3aef63f2a0442221a4f
friendou <robert.dmc@hotmail.com>
2020-03-01 00:28:57 -0400

    Removing moduels from app moduel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
-import { CerealsModule } from './cereals/cereals.module';
-import { SnacksModule } from './snacks/snacks.module';
 import { LayoutModule } from './layout/layout.module';
 import { SharedModule } from './shared/shared.module';
 
@@ -17,8 +15,6 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
-    CerealsModule,
-    SnacksModule,
     SharedModule,
   ],
   providers: [],
